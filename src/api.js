ce127680629c8f8f0fe5be02ebb0e8f8d2cdccfb
calebchris000 <calebchris000@gmail.com>
2023-03-07 10:41:36 +0100

    complete api for send and receive

--- src/api.js
+++ src/api.js
@@ -1,12 +1,13 @@
 const postData = async (user, score) => {
-  const send = await fetch("http://localhost:3000/profile ", {
+  const send = await fetch("http://localhost:3000/posts ", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ user: user, score: score }),
   });
   let response = await send.json();
   return response;
-};
+}; 
+
 
 const getData = async () => {
   const send = await fetch("http://localhost:3000/posts");
