8c7447f5dec94db2cbe7db482a8caadf55393edf
calebchris000 <calebchris000@gmail.com>
2023-03-07 22:07:03 +0100

    fix linters

--- src/api.js
+++ src/api.js
@@ -1,17 +1,17 @@
 export const postData = async (user, score) => {
   await fetch(
-    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/zgU632WsHpsxMoPC2L4Z/scores",
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/zgU632WsHpsxMoPC2L4Z/scores',
     {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ user, score }),
-    }
+    },
   );
 };
 
 export const getData = async () => {
   const send = await fetch(
-    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/zgU632WsHpsxMoPC2L4Z/scores"
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/zgU632WsHpsxMoPC2L4Z/scores',
   );
   const response = await send.json();
   return response;
