014c74720f48be5029d858063839954fd62da970
calebchris000 <calebchris000@gmail.com>
2023-03-07 10:41:53 +0100

    Done with HTML

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const { postData, getData } = require("./api.js");
+const createList = require("./createLists.js");
+require("./index.scss");
+
+const user = document.getElementById("user");
+const score = document.getElementById("score");
+const submit = document.getElementById("submit");
+const upload = async (e) => {
+  e.preventDefault();
+  await postData(user.value, score.value);
+  user.value = "";
+  score.value = "";
+
+  let display = await getData();
+  createList(display);
+};
+
+submit.addEventListener("click", upload);
+
+const reload = async () => {
+  const get = await getData();
+  createList(get);
+};
+
+window.onload = reload;
