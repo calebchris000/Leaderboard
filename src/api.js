05ecd0105423bf146a190bc38993ce2f7248005c
calebchris000 <calebchris000@gmail.com>
2023-03-07 22:04:38 +0100

    Remove useless variables

--- src/api.js
+++ src/api.js
@@ -1,18 +1,18 @@
 export const postData = async (user, score) => {
-  const send = await fetch(
-    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/zgU632WsHpsxMoPC2L4Z/scores',
+  await fetch(
+    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/zgU632WsHpsxMoPC2L4Z/scores",
     {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ user, score }),
-    },
+    }
   );
-  const response = await send.json();
-  return response;
 };
 
 export const getData = async () => {
-  const send = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/zgU632WsHpsxMoPC2L4Z/scores');
+  const send = await fetch(
+    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/zgU632WsHpsxMoPC2L4Z/scores"
+  );
   const response = await send.json();
   return response;
 };
