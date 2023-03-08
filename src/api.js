56822e2e88c9b9c24d27dcfc6e35196afdd1c604
calebchris000 <calebchris000@gmail.com>
2023-03-08 19:31:58 +0100

    Add limit to input characters, change API id, as original was being spammed

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 export const postData = async (user, score) => {
   await fetch(
-    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/zgU632WsHpsxMoPC2L4Z/scores',
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/v7mmYfE3wwDGVK2OfzPD/scores',
     {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -11,7 +11,7 @@ export const postData = async (user, score) => {
 
 export const getData = async () => {
   const send = await fetch(
-    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/zgU632WsHpsxMoPC2L4Z/scores',
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/v7mmYfE3wwDGVK2OfzPD/scores',
   );
   const response = await send.json();
   return response;
