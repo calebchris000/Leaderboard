ff38eefe35116fa4e487e36861f3a219b917b6f9
calebchris000 <calebchris000@gmail.com>
2023-03-09 07:31:46 +0100

    Fix linters

--- src/createLists.js
+++ src/createLists.js
@@ -6,8 +6,8 @@ const createList = (data) => {
 
   sorted.forEach((element) => {
     const { user, score } = element;
-    let slice1 = user.slice(0, 10)
-    let slice2 = score.slice(0, 2)
+    const slice1 = user.slice(0, 10);
+    const slice2 = score.slice(0, 2);
     item += `
     
     <div class="item">${slice1}: ${slice2}</div>
